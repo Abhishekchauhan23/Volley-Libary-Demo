63120c734ba4c27597d17fe706fdc7d069e91937
Abhishek <abhishekchauhan2277@gmail.com>
2020-09-27 20:37:02 +0530

    Initial Build

---
+++ app/src/main/java/com/example/volleypractice/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.volleypractice
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadMeme()
+    }
+    fun loadMeme(){
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            Response.Listener { response ->
+                val url=response.getString("url")
+                Glide.with(this).load(url).listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        TODO("Not yet implemented")
+                    }
+
+                }).into(memeImage)
+            },
+            Response.ErrorListener { })
+
+// Add the request to the RequestQueue.
+        queue.add(jsonRequest)
+    }
+    fun shareMeme(view: View) {}
+    fun nextMeme(view: View) {loadMeme()}
+}
